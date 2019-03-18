e605c41855a42dccee40d2fa221f171b4a843642
meeranarayanan <47768240+meeranarayanan@users.noreply.github.com>
2019-03-18 13:09:59 +0530

    Update accept.php

--- accept.php
+++ accept.php
@@ -4,7 +4,6 @@ $id=$_GET['id'];
 $i=mysql_query("update login set Usertype='user' where Logid='$id'");
  ?>
 <script>
-ssss
 alert("update successfully");
 window.location="acceptandblock.php";
 </script>
